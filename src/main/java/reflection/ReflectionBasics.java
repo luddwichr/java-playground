703fe3f6b3c5a54f5f1f73468dee037313dacc33
Ludwig Richter <ludwig.richter@andrena.de>
2020-04-14 15:56:32 +0200

    improve readability

--- src/main/java/reflection/ReflectionBasics.java
+++ src/main/java/reflection/ReflectionBasics.java
@@ -11,78 +11,77 @@ import static java.util.stream.Collectors.toList;
 // see https://www.baeldung.com/java-reflection for more on how reflection can be used
 public class ReflectionBasics {
 
-	public static <T> T createInstance(Class<T> clazz, List<Object> parameters) {
-		Class<?>[] parameterTypes = parameters.stream().map(Object::getClass).toArray(Class<?>[]::new);
-		try {
-			Constructor<T> constructor = clazz.getConstructor(parameterTypes);
-			return constructor.newInstance(parameters.toArray());
-		} catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static <T> T createInstance(Class<T> clazz, List<Object> parameters) {
+        Class<?>[] parameterTypes = parameters.stream().map(Object::getClass).toArray(Class<?>[]::new);
+        try {
+            Constructor<T> constructor = clazz.getConstructor(parameterTypes);
+            return constructor.newInstance(parameters.toArray());
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static List<String> getNamesOfPrivateFields(Object object) {
-		return Stream.of(object.getClass().getDeclaredFields())
-				.map(Field::getName)
-				.collect(toList());
-	}
+    public static List<String> getNamesOfFields(Object object) {
+        return Stream.of(object.getClass().getDeclaredFields())
+                .map(Field::getName)
+                .collect(toList());
+    }
 
-	public static List<Object> getValuesOfPrivateFields(Object object) {
-		return Stream.of(object.getClass().getDeclaredFields())
-				.peek(field -> field.setAccessible(true))
-				.map(field -> getFieldValue(field, object))
-				.collect(toList());
-	}
+    public static List<Object> getValuesOfFields(Object object) {
+        return Stream.of(object.getClass().getDeclaredFields())
+                .peek(field -> field.setAccessible(true))
+                .map(field -> getFieldValue(field, object))
+                .collect(toList());
+    }
 
-	private static Object getFieldValue(Field field, Object object) {
-		try {
-			return field.get(object);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private static Object getFieldValue(Field field, Object object) {
+        try {
+            return field.get(object);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static void changeValuesOfPrivateFinalFields(Object object, Map<String, Object> valuePerField) {
-		for (Map.Entry<String, Object> entry : valuePerField.entrySet()) {
-			try {
-				Field field = object.getClass().getDeclaredField(entry.getKey());
-				field.setAccessible(true);
-				field.set(object, entry.getValue());
-			} catch (NoSuchFieldException | IllegalAccessException e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
+    public static void changeValuesOfPrivateFinalFields(Object object, Map<String, Object> valuePerField) {
+        for (Map.Entry<String, Object> entry : valuePerField.entrySet()) {
+            try {
+                Field field = object.getClass().getDeclaredField(entry.getKey());
+                field.setAccessible(true);
+                field.set(object, entry.getValue());
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
-	public static void changeTypesOfPrivateFinalFields(Object object, Map<String, Object> valuePerField) {
-		for (Map.Entry<String, Object> entry : valuePerField.entrySet()) {
-			try {
-				Field field = object.getClass().getDeclaredField(entry.getKey());
+    public static void changeTypesOfPrivateFinalFields(Object object, Map<String, Object> valuePerField) {
+        for (Map.Entry<String, Object> entry : valuePerField.entrySet()) {
+            try {
+                Field field = object.getClass().getDeclaredField(entry.getKey());
 
-				Field modifiersField = Field.class.getDeclaredField("modifiers");
-				modifiersField.setAccessible(true);
-				modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+                Field modifiersField = Field.class.getDeclaredField("modifiers");
+                modifiersField.setAccessible(true);
+                modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
-				Field fieldType = Field.class.getDeclaredField("type");
-				fieldType.setAccessible(true);
-				fieldType.set(field, entry.getValue().getClass());
+                Field fieldType = Field.class.getDeclaredField("type");
+                fieldType.setAccessible(true);
+                fieldType.set(field, entry.getValue().getClass());
 
-				field.setAccessible(true);
-				field.set(object, entry.getValue());
-			} catch (NoSuchFieldException | IllegalAccessException e) {
-				throw new RuntimeException(e);
-			}
-		}
+                field.setAccessible(true);
+                field.set(object, entry.getValue());
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
-	}
+    public static List<String> getMethodNames(Object object) {
+        return Stream.of(object.getClass().getDeclaredMethods()).map(Method::getName).collect(toList());
+    }
 
-	public static List<String> getMethodNames(Object object) {
-		return Stream.of(object.getClass().getDeclaredMethods()).map(Method::getName).collect(toList());
-	}
-
-	public static List<Class<?>> getAnnotationClasses(Object object) {
-		return Stream.of(object.getClass().getDeclaredAnnotations()).map(Annotation::annotationType).collect(toList());
-	}
+    public static List<Class<?>> getAnnotationClasses(Object object) {
+        return Stream.of(object.getClass().getDeclaredAnnotations()).map(Annotation::annotationType).collect(toList());
+    }
 
 }
 
--- src/test/java/reflection/ReflectionBasicsTest.java
+++ src/test/java/reflection/ReflectionBasicsTest.java
@@ -11,90 +11,95 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ReflectionBasicsTest {
 
-	@Test
-	void createInstance() {
-		List<Object> parameters = Arrays.asList("Hans", 99, true);
-		Person person = ReflectionBasics.createInstance(Person.class, parameters);
-		assertThat(person).isEqualToComparingFieldByField(new Person("Hans", 99, true));
-	}
+    @Test
+    void createInstance() {
+        List<Object> parameters = Arrays.asList("Hans", 99, true);
+        Person person = ReflectionBasics.createInstance(Person.class, parameters);
+        assertThat(person).isEqualToComparingFieldByField(new Person("Hans", 99, true));
+    }
 
-	@Test
-	void getMethodNames() {
-		assertThat(ReflectionBasics.getMethodNames(new Person("Erna", 42, true)))
-				.contains("getName", "setName", "getMarried", "setMarried", "getAge", "setAge")
-				.contains("protectedMethod", "packagePrivateMethod", "privateMethod");
-	}
+    @Test
+    void getMethodNames() {
+        Person person = new Person("Erna", 42, true);
+        List<String> methodNames = ReflectionBasics.getMethodNames(person);
+        assertThat(methodNames)
+                .contains("getName", "setName", "getMarried", "setMarried", "getAge", "setAge")
+                .contains("protectedMethod", "packagePrivateMethod", "privateMethod");
+    }
 
-	@Test
-	void getAnnotationClasses() {
-		assertThat(ReflectionBasics.getAnnotationClasses(new Person("Erna", 42, true)))
-				.containsExactly(AnnotationForRuntime.class);
-	}
+    @Test
+    void getAnnotationClasses() {
+        Person person = new Person("Erna", 42, true);
+        List<Class<?>> annotationClasses = ReflectionBasics.getAnnotationClasses(person);
+        assertThat(annotationClasses).containsExactly(AnnotationForRuntime.class);
+    }
 
-	@Test
-	void getNamesOfPrivateFields() {
-		assertThat(ReflectionBasics.getNamesOfPrivateFields(new Person("Erna", 42, true)))
-				.containsExactlyInAnyOrder("name", "age", "married");
-	}
+    @Test
+    void getNamesOfFields() {
+        Person person = new Person("Erna", 42, true);
+        List<String> namesOfFields = ReflectionBasics.getNamesOfFields(person);
+        assertThat(namesOfFields).containsExactlyInAnyOrder("name", "age", "married");
+    }
 
-	@Test
-	void getValuesOfPrivateFields() {
-		assertThat(ReflectionBasics.getValuesOfPrivateFields(new Person("Erna", 42, true)))
-				.containsExactlyInAnyOrder("Erna", 42, true);
-	}
+    @Test
+    void getValuesOfFields() {
+        Person person = new Person("Erna", 42, true);
+        List<Object> valuesOfFields = ReflectionBasics.getValuesOfFields(person);
+        assertThat(valuesOfFields).containsExactlyInAnyOrder("Erna", 42, true);
+    }
 
-	@Test
-	void changeValuesOfPrivateFinalFields() {
-		ClassForPrivateFinalFieldValueChanges instance = new ClassForPrivateFinalFieldValueChanges("A", 321, true);
-		Map<String, Object> valuePerField = new HashMap<>();
-		valuePerField.put("string", "B");
-		valuePerField.put("integer", 123);
-		valuePerField.put("aBoolean", false);
+    @Test
+    void changeValuesOfPrivateFinalFields() {
+        ClassForPrivateFinalFieldValueChanges instance = new ClassForPrivateFinalFieldValueChanges("A", 321, true);
+        Map<String, Object> valuePerField = new HashMap<>();
+        valuePerField.put("string", "B");
+        valuePerField.put("integer", 123);
+        valuePerField.put("aBoolean", false);
 
-		ReflectionBasics.changeValuesOfPrivateFinalFields(instance, valuePerField);
+        ReflectionBasics.changeValuesOfPrivateFinalFields(instance, valuePerField);
 
-		assertThat(instance).isEqualToComparingFieldByField(new ClassForPrivateFinalFieldValueChanges("B", 123, false));
-	}
+        assertThat(instance).isEqualToComparingFieldByField(new ClassForPrivateFinalFieldValueChanges("B", 123, false));
+    }
 
-	@Test
-	void changeTypeOfPrivateFinalFields() {
-		ClassForPrivateFinalFieldTypeChanges instance = new ClassForPrivateFinalFieldTypeChanges("A", 321, true);
+    @Test
+    void changeTypeOfPrivateFinalFields() {
+        ClassForPrivateFinalFieldTypeChanges instance = new ClassForPrivateFinalFieldTypeChanges("A", 321, true);
 
-		Map<String, Object> valuePerField = new HashMap<>();
-		valuePerField.put("string", 123);
-		valuePerField.put("integer", false);
-		valuePerField.put("aBoolean", "yes");
+        Map<String, Object> valuePerField = new HashMap<>();
+        valuePerField.put("string", 123);
+        valuePerField.put("integer", false);
+        valuePerField.put("aBoolean", "yes");
 
-		ReflectionBasics.changeTypesOfPrivateFinalFields(instance, valuePerField);
+        ReflectionBasics.changeTypesOfPrivateFinalFields(instance, valuePerField);
 
-		assertThat(ReflectionBasics.getValuesOfPrivateFields(instance))
-				.containsExactlyInAnyOrder(123, false, "yes");
-	}
+        assertThat(ReflectionBasics.getValuesOfFields(instance))
+                .containsExactlyInAnyOrder(123, false, "yes");
+    }
 
 }
 
 class ClassForPrivateFinalFieldValueChanges {
 
-	private final String string;
-	private final Integer integer;
-	private final Boolean aBoolean;
+    private final String string;
+    private final Integer integer;
+    private final Boolean aBoolean;
 
-	public ClassForPrivateFinalFieldValueChanges(String string, Integer integer, Boolean aBoolean) {
-		this.string = string;
-		this.integer = integer;
-		this.aBoolean = aBoolean;
-	}
+    public ClassForPrivateFinalFieldValueChanges(String string, Integer integer, Boolean aBoolean) {
+        this.string = string;
+        this.integer = integer;
+        this.aBoolean = aBoolean;
+    }
 }
 
 class ClassForPrivateFinalFieldTypeChanges {
 
-	private final String string;
-	private final Integer integer;
-	private final Boolean aBoolean;
+    private final String string;
+    private final Integer integer;
+    private final Boolean aBoolean;
 
-	public ClassForPrivateFinalFieldTypeChanges(String string, Integer integer, Boolean aBoolean) {
-		this.string = string;
-		this.integer = integer;
-		this.aBoolean = aBoolean;
-	}
+    public ClassForPrivateFinalFieldTypeChanges(String string, Integer integer, Boolean aBoolean) {
+        this.string = string;
+        this.integer = integer;
+        this.aBoolean = aBoolean;
+    }
 }
